ea006faa1af2ee9da5e264d75d2fbc3160894930
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2024-03-07 04:50:26 +0530

    feat(main.py): convert to api
    converted to api using flask

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
 from StethoConnect import StethoConnect
-
 import asyncio
 
-async def main():
-    steth = StethoConnect()
-    await steth.record_audio()
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
+@app.route('/record', methods=['POST'])
+def record():
+    async def record_audio():
+        steth = StethoConnect()
+        await steth.record_audio()
+    
+    # Run the main coroutine
+    asyncio.run(record_audio())
 
-asyncio.run(main())
+    return "Recording started"
+
+if __name__ == '__main__':
+    app.run(debug=True)
